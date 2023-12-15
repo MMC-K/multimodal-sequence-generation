36a6a9afe42f15ccdfb11dc6532e7f5c4e882ee2
san <kimsan0622@gmail.com>
2023-12-15 16:35:44 +0900

    add seq2latent

--- cvt_seq2latent.py
+++ cvt_seq2latent.py
@@ -1,3 +1,5 @@
+import argparse
+
 from datasets import load_dataset, load_from_disk
 
 from transformers import AutoTokenizer
@@ -6,31 +8,52 @@ import torch
 from tqdm import tqdm
 from vqvae import VectorQuantizedVAE
 
-raw_datasets = load_from_disk("preprocessed_data/proc")
 
-model = VectorQuantizedVAE(153, 256, 256, 2048)
-model.load_state_dict(torch.load("results/vqvae_1209_prec/step_28000/pytorch_model.bin"))
-model.eval()
-print("model loaded!!")
-
-@torch.no_grad()
-def preroc_fn(examples):
-    new_pose_feature = [model.encode(torch.tensor(np.array(pf).reshape((1, -1, 153)), dtype=torch.float32)).tolist()[0] for pf in examples["pose_feature"]]
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--saved_dataset_path", type=str, default="preproc_data/proc", help="The path to saved data")
+    parser.add_argument("--vqvae_model_path", type=str, default="test_output2/proc_shard10/pytorch_model.bin", help="The path to vqvae data")
+    parser.add_argument("--output_path", type=str, default="preproc_data/proc_idx", help="The path to output directory")
     
-    return {
-        "index": new_pose_feature,
-        "text": examples["text"]
-    }
+    parser.add_argument("--vq_codebook_size", type=int, default=2048, help="vq_codebook_size")
+    parser.add_argument("--vq_input_size", type=int, default=153, help="vq_input_size")
+    parser.add_argument("--vq_hidden_size", type=int, default=256, help="vq_hidden_size")
+    parser.add_argument("--vq_output_size", type=int, default=256, help="vq_output_size")
+    return parser.parse_args()
 
-train_columns = raw_datasets["train"].column_names
+
+def main():
+    args = parse_args()
+
+    raw_datasets = load_from_disk(args.saved_dataset_path)
+
+    model = VectorQuantizedVAE(args.vq_input_size, args.vq_hidden_size, args.vq_output_size, args.vq_codebook_size)
+    model.load_state_dict(torch.load(args.vqvae_model_path))
+    model.eval()
+
+    @torch.no_grad()
+    def preroc_fn(examples):
+        new_pose_feature = [model.encode(torch.tensor(np.array(pf).reshape((1, -1, 153)), dtype=torch.float32)).tolist()[0] for pf in examples["pose_feature"]]
+        
+        return {
+            "index": new_pose_feature,
+            "text": examples["text"]
+        }
+
+    train_columns = raw_datasets["train"].column_names
+        
+    raw_datasets = raw_datasets.map(
+        preroc_fn, 
+        batched=True,
+        batch_size=250,
+        remove_columns=["pose_feature"], 
+        num_proc=4, 
+        desc="indexing..."
+    )
+
+    raw_datasets.save_to_disk(args.output_path)
+
+if __name__=="__main__":
+    main()
     
-raw_datasets = raw_datasets.map(
-    preroc_fn, 
-    batched=True,
-    batch_size=250,
-    remove_columns=["pose_feature"], 
-    num_proc=4, 
-    desc="indexing..."
-)
-
-raw_datasets.save_to_disk("preprocessed_data/proc_idx_text")
+    
